15d8e7715d05a5907e45078a6317be20887ff96d
masayakaneko <kane.coma@gmail.com>
2020-05-19 11:40:38 +0900

    deleteUser
    チャットメンバーの削除機能追加

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,7 +9,6 @@ $(function() {
       `
       $("#user-search-result").append(html);
   }
-
   function addNoUser() {
     let html = `
       <div class="chat-group-user clearfix">
@@ -17,6 +16,19 @@ $(function() {
       </div>
     `
     $("#user-search-result").append(html);
+  }
+
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
   }
 
   $("#user-search-field").on("keyup", function() {
@@ -33,8 +45,6 @@ $(function() {
           users.forEach(function(user){
             addUser(user);
           });
-        } else if (input.length == 0) {
-          return false;
         } else {
           addNoUser();
         }
@@ -42,5 +52,20 @@ $(function() {
       .fail(function() {
         alert("通信エラーです。ユーザーが表示できません。");
       });
+  });
+
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
   });
 });
