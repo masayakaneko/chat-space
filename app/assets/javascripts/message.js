feeecf29fadd32b39e762a56b0935330b81066ce
masayakaneko <kane.coma@gmail.com>
2020-05-21 13:06:37 +0900

    reloadMessages
    jsファイルにて更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){
+
   function buildHTML(message) {
     if (message.image) {
       var html =
-        `<div class = "message">
+        `<div class = "message" data-message-id=${message.id}>
           <div class = "message__name">
             ${message.user_name}
           </div>
@@ -19,7 +20,7 @@ $(function(){
     return html;
     } else {
       var html =
-        `<div class = "message">
+        `<div class = "message" data-message-id=${message.id}>
           <div class = "message__name">
             ${message.user_name}
           </div>
@@ -43,20 +44,46 @@ $(function(){
     $.ajax({
       url: url,
       type: "POST",
-      data: formData,  
+      data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.main-messages').append(html);
-      $('.main-messages').animate({ scrollTop: $('.main-messages')[0].scrollHeight});
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('form')[0].reset();
       $('.form__submit').prop('disabled', false);
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     });
-  });
+  })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('自動更新されませんでした');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 500);
+  }
 });
