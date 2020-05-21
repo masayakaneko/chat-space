0043c808740800a80695ae41e58db5bf4e85c74e
masayakaneko <kane.coma@gmail.com>
2020-05-21 10:26:08 +0900

    automatic
    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,17 @@
 $(function(){
+
   function buildHTML(message) {
     if (message.image) {
       var html =
-        `<div class = "message-wrapper">
-          <div class = "message-wrapper__name">
+        `<div class = "message" data-message-id=${message.id}>
+          <div class = "message__name">
             ${message.user_name}
           </div>
-          <div class = "message-wrapper__date">
+          <div class = "message__date">
             ${message.created_at}
           </div>
-          <div class = "message-wrapper__under">
-            <p class="message-wrapper__under__text">
+          <div class = "message__under">
+            <p class="message__under__text">
               ${message.content}
             </p>
             <img class="lower-message__image" src="${message.image}">
@@ -19,15 +20,15 @@ $(function(){
     return html;
     } else {
       var html =
-        `<div class = "message-wrapper">
-          <div class = "message-wrapper__name">
+        `<div class = "message" data-message-id=${message.id}>
+          <div class = "message__name">
             ${message.user_name}
           </div>
-          <div class = "message-wrapper__date">
+          <div class = "message__date">
             ${message.created_at}
           </div>
-          <div class = "message-wrapper__under">
-            <p class="message-wrapper__under__text">
+          <div class = "message__under">
+            <p class="message__under__text">
               ${message.content}
             </p>
           </div>
@@ -43,20 +44,47 @@ $(function(){
     $.ajax({
       url: url,
       type: "POST",
-      data: formData,  
+      data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.main-messages').append(html);
-      $('.main-messages').animate({ scrollTop: $('.main-messages')[0].scrollHeight});
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('form')[0].reset();
       $('.form__submit').prop('disabled', false);
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     });
-  });
+  })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('自動更新されませんでした');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 500);
+  }
 });
